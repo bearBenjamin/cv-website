ff205db65952bc4535f1bade5d8130f39ed3f797
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-05-23 22:56:58 +0300

    feat: сверстал и стилизовал header, написал логику бургер меню

---
+++ source/scripts/burger-menu.js
@@ -0,0 +1,48 @@
+const body = document.documentElement.querySelector('body');
+const menuNav = document.querySelector('.header-list');
+const btnBurger = document.querySelector('.btn-header-burger');
+const btnBurgerText = document.querySelector('.btn-header-burger-text');
+
+const closeMenu = () => {
+  body.classList.remove('no-scroll');
+  menuNav.classList.remove('menu-open');
+  btnBurger.classList.remove('btn-header-burger-close');
+  btnBurgerText.textContent = 'Открыть меню';
+};
+
+const openMenu = () => {
+  body.classList.add('no-scroll');
+  menuNav.classList.add('menu-open');
+  btnBurger.classList.add('btn-header-burger-close');
+  btnBurgerText.textContent = 'Закрыть меню';
+};
+
+const initNavigation = () => {
+  if (!menuNav) {
+    return;
+  }
+
+  menuNav.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('header-link')) {
+      closeMenu();
+    }
+  });
+};
+
+const initMenu = () => {
+  if (!btnBurger) {
+    return;
+  }
+
+  btnBurger.addEventListener('click', () => {
+    if (!btnBurger.classList.contains('btn-header-burger-close')) {
+      openMenu();
+    } else {
+      closeMenu();
+    }
+  });
+
+  initNavigation();
+};
+
+export { initMenu };
