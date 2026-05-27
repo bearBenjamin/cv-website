6025640286844e5b6d82f26b1c73b0953f743c0c
Ilia Konev <122528494+bearBenjamin@users.noreply.github.com>
2026-05-27 11:47:57 +0300

    feat: написал разметку, стили и логику секции Project

---
+++ source/scripts/data.js
@@ -0,0 +1,43 @@
+
+const additionalProjects = [
+  {
+    name: 'Интернет магазин: Cat Energy',
+    tags: 'HTML / SASS(SCSS) / БЭМ / JavaScript',
+    description: 'Интернет-магазин функционального питания котов. Выполнен в рамках обучения на курсе “HTML и CSS адаптивная верстка сайтов и оптимизация” HTML-Academy.',
+    github: 'https://github.com/bearBenjamin/2274609-cat-energy-2',
+    live: 'https://bearbenjamin.github.io/2274609-cat-energy-2/',
+    figma: 'https://www.figma.com/design/kwmtyIigKr8bhkgfoAdHGU/HTML-2---%D0%9A%D1%8D%D1%82-%D1%8D%D0%BD%D0%B5%D1%80%D0%B4%D0%B6%D0%B8--31---Copy-?node-id=0-1&p=f&t=Vxm7nDpJKsswQUmh-0'
+  },
+  {
+    name: 'Portfolio',
+    tags: 'HTML / CSS / JavaScript',
+    description: 'Одностраничный лэндинг фотографа-фрилансера. Выполнен в рамках самостоятельного обучения на Stage 1 RS-school в закрытом репозитории школы.  В проекте реализовал отзывчивую (responsive) верстку и интерактивный слайдер с автоматической прокруткой фотографий.” HTML-Academy.',
+    github: '#',
+    live: 'https://rolling-scopes-school.github.io/bearbenjamin-JSFE2025Q3/portfolio/',
+    figma: 'https://www.figma.com/design/MKwZw6cPoWdpOCXOkrSsum/Portfolio--Copy-?t=Vxm7nDpJKsswQUmh-0'
+  },
+  {
+    name: 'Pair-em-Up',
+    tags: 'HTML / CSS / JavaScript',
+    description: 'Стратегическая головоломка (браузерная игра) на сопоставление чисел. Выполнена в рамках самостоятельного обучения на Stage 1 RS-school. Приложение реализовано на чистом JavaScript с динамическим манипулированием DOM (SPA на “ванили”). Сохранение состояний игры, настроек и статистики осуществляется в localStorage.',
+    github: 'https://github.com/bearBenjamin/pair-em-up',
+    live: 'https://bearbenjamin.github.io/pair-em-up/'
+  },
+  {
+    name: 'Sedona',
+    tags: 'HTML / CSS ',
+    description: 'Информационный портал г.Седона для туристов. Выполнен в рамках обучения на курсе “HTML и CSS профессиональная верстка сайтов” HTML-Academy.',
+    github: 'https://github.com/bearBenjamin/2274609-sedona-2',
+    live: 'https://bearbenjamin.github.io/2274609-sedona-2/',
+    figma: 'https://www.figma.com/design/ojPEH63emdZbVk85VmMw8t/HTML-1---%D0%A1%D0%B5%D0%B4%D0%BE%D0%BD%D0%B0--39---Copy-?t=Vxm7nDpJKsswQUmh-0'
+  },
+  {
+    name: 'CV',
+    tags: 'HTML / CSS ',
+    description: 'Первый вариант сайта-визитки, выполнен в рамках обучения на Stage 0 RS-school',
+    github: 'https://github.com/bearBenjamin/rsschool-cv',
+    live: 'https://bearbenjamin.github.io/rsschool-cv/'
+  }
+];
+
+export { additionalProjects };
---
+++ source/scripts/project-loading.js
@@ -0,0 +1,173 @@
+import { additionalProjects } from './data.js';
+
+const portfolio = document.querySelector('.portfolio-section');
+const btnMore = portfolio.querySelector('.btn-more-projects');
+const btnLess = portfolio.querySelector('.btn-less-projects');
+const accordion = portfolio.querySelector('.accordion');
+const projects = portfolio.querySelectorAll('.accordion-item');
+
+const parseHtmlProjects = [...projects].map((project) => {
+
+  const nameElem = project.querySelector('.project-name');
+  const tagsElem = project.querySelector('.project-tags');
+  const descriptionElem = project.querySelector('.project-description');
+
+  const projectObject = {
+    name: nameElem ? nameElem.textContent.trim() : '',
+    tags: tagsElem ? tagsElem.textContent.trim() : '',
+    description: descriptionElem ? descriptionElem.textContent.trim() : '',
+  };
+
+  const gitHubLink = project.querySelector('a[href*="github.com"]');
+  const liveLink = project.querySelector('a[href*="github.io"]');
+  const figmaLink = project.querySelector('a[href*="figma.com"]');
+
+  if (gitHubLink) {
+    projectObject.github = gitHubLink.getAttribute('href');
+  }
+
+  if (liveLink) {
+    projectObject.live = liveLink.getAttribute('href');
+  }
+
+  if (figmaLink) {
+    projectObject.figma = figmaLink.getAttribute('href');
+  }
+
+  return projectObject;
+});
+
+
+const allProjects = [...parseHtmlProjects, ...additionalProjects];
+const length = allProjects.length;
+
+let indexStart = 5;
+let indexEnd = 10;
+
+const initProjectsLoader = () => {
+
+  if (!btnMore || !btnLess || !accordion) {
+    return;
+  }
+
+  const currentEnd = Math.min(indexEnd, length);
+
+  for (let i = indexStart; i < currentEnd; i += 1) {
+    const details = document.createElement('details');
+    details.classList.add('accordion-item');
+
+    const summary = document.createElement('summary');
+    summary.classList.add('accordion-header');
+
+    const spanName = document.createElement('span');
+    spanName.classList.add('project-name');
+    spanName.dataset.i18n = `name__project__${String(i + 1).padStart(2, '0')}`;
+    spanName.textContent = `${String(i + 1).padStart(2, '0')}. ${allProjects[i].name}`;
+
+    summary.append(spanName);
+
+    const spanTag = document.createElement('span');
+    spanTag.classList.add('project-tags');
+    spanTag.textContent = allProjects[i].tags;
+
+    summary.append(spanTag);
+
+    const spanIcon = document.createElement('span');
+    spanIcon.classList.add('accordion-icon');
+
+    summary.append(spanIcon);
+    details.append(summary);
+
+    const divContent = document.createElement('div');
+    divContent.classList.add('accordion-content');
+
+    const description = document.createElement('p');
+    description.classList.add('project-description');
+    description.dataset.i18n = `project__description__${String(i + 1).padStart(2, '0')}`;
+    description.textContent = allProjects[i].description;
+
+    divContent.append(description);
+
+    const divLink = document.createElement('div');
+    divLink.classList.add('project-links');
+
+    if (allProjects[i].github) {
+      const gitLink = document.createElement('a');
+      gitLink.classList.add('btn-link');
+      gitLink.href = allProjects[i].github;
+      gitLink.target = '_blank';
+      gitLink.rel = 'noopener noreferrer';
+      gitLink.textContent = 'GitHub';
+
+      divLink.append(gitLink);
+    }
+
+    if(allProjects[i].live) {
+      const liveLink = document.createElement('a');
+      liveLink.classList.add('btn-link');
+      liveLink.href = allProjects[i].live;
+      liveLink.target = '_blank';
+      liveLink.rel = 'noopener noreferrer';
+      liveLink.textContent = 'Live Demo';
+
+      divLink.append(liveLink);
+    }
+
+    if(allProjects[i].figma) {
+      const figmaLink = document.createElement('a');
+      figmaLink.classList.add('btn-link');
+      figmaLink.href = allProjects[i].figma;
+      figmaLink.target = '_blank';
+      figmaLink.rel = 'noopener noreferrer';
+      figmaLink.textContent = 'Figma';
+
+      divLink.append(figmaLink);
+    }
+
+    divContent.append(divLink);
+    details.append(divContent);
+    accordion.append(details);
+  }
+};
+
+btnMore.addEventListener('click', () => {
+  initProjectsLoader();
+  indexStart += 5;
+  indexEnd += 5;
+
+  if(indexStart >= length) {
+    btnMore.disabled = true;
+  }
+
+  btnLess.classList.remove('visually-hidden');
+  btnLess.classList.add('is-visible');
+});
+
+btnLess.addEventListener('click', () => {
+  const allCurrentItems = accordion.querySelectorAll('.accordion-item');
+
+  if (allCurrentItems.length <= 5) {
+    return;
+  }
+
+  const firstDynamicItem = allCurrentItems[5];
+
+  if (firstDynamicItem) {
+    firstDynamicItem.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+
+  for (let i = 5; i < allCurrentItems.length; i += 1) {
+    if (allCurrentItems[i]) {
+      allCurrentItems[i].remove();
+    }
+  }
+
+  indexStart = 5;
+  indexEnd = 10;
+
+  btnMore.disabled = false;
+
+  btnLess.classList.remove('is-visible');
+  btnLess.classList.add('visually-hidden');
+
+});
